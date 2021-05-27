7319b3cda0ca57f33531106dd4bf04d1e83a7a12
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-05-28 00:20:25 +0900

    자료구조와 알고리즘 과제2
    [과제2]
    
    문자열들 데이터가 들어오면 그것을 합병정렬해서 출력하는 프로그램을 작성합니다.
    
    예를 들어서
    
    입력이
    
    5
    
    cde
    
    abcd
    
    rhk
    
    bcd
    
    xyz
    
    들어오면
    
    abcd
    
    bcd
    
    cde
    
    rhk
    
    xyz
    
    를 출력하면 됩니다.
    
    데이터 입력갯수는 최대 1,000개가 들어올 수 있다고 가정토록 하고
    
    문자열은 최대 길이가 100글자인 것으로 가정합니다.
    
    모든 함수는 직접 작성토록 합니다.
    
    중복문자열 들어올시 출력안됨 예외처리 필요

---
+++ Algortitm_Tark_4/Two.java
@@ -0,0 +1,82 @@
+package Algortitm_Tark_4;
+
+import java.util.Scanner;
+
+public class Two {
+	static void mergeSort(String[] arr) {
+		String[] tmp = new String[arr.length];
+		mergeSort(arr,tmp,0,arr.length-1);
+		
+	}
+	static void mergeSort(String[]arr,String[]tmp,int start,int end) {
+		
+		if(start<end) {
+		int mid = (start+end)/2;
+		mergeSort(arr,tmp,start,mid);
+		mergeSort(arr,tmp,mid+1,end);
+		merge(arr,tmp,start,mid,end);
+	
+		}
+	}
+	static void merge(String[] arr, String[] tmp, int start, int mid, int end) {
+		for(int i =0; i<arr.length; i++) {
+			tmp[i] = arr[i];
+		}
+		int part1 = start;
+		int part2 = mid+1;
+		int index = start;
+		while(part1<=mid && part2<=end) {
+		if((int)tmp[part1].charAt(0) < (int)tmp[part2].charAt(0)) {
+				arr[index] = tmp[part1];
+				part1 ++;
+			}
+			else if((int)tmp[part1].charAt(0) > (int)tmp[part2].charAt(0)) {
+				arr[index] = tmp[part2];
+				part2 ++;
+			}
+			
+			else {
+				int addindex = 0;
+				if(addindex > tmp[part1].length() || addindex>tmp[part2].length()) {
+					break;
+				}
+				else {
+				while((int)tmp[part1].charAt(addindex) < (int)tmp[part2].charAt(addindex) ||
+					(int)tmp[part1].charAt(addindex) > (int)tmp[part2].charAt(addindex)) {
+					addindex++;
+				}
+				if((int)tmp[part1].charAt(addindex) < (int)tmp[part2].charAt(addindex)) {
+					arr[index] = tmp[part1];
+					part1 ++;
+					break;
+				}
+				else if((int)tmp[part1].charAt(addindex) > (int)tmp[part2].charAt(addindex)) {
+					arr[index] = tmp[part2];
+					part2 ++;
+					break;
+				}
+				}
+			}
+			index++;
+		}
+		for(int i =0; i<=mid-part1;i++) {
+			arr[index+i] = tmp[part1+i];
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] arr = new String[n];
+		for(int i = 0; i<n; i++) {
+			arr[i] = sc.next();
+		}
+		mergeSort(arr);
+		for(int i =0; i <arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		
+	}
+
+}
